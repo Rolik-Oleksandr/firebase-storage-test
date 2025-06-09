375d7216a47889a0a85b6c754155cdbf317509bd
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-09 22:03:56 +0300

    Initial Commit

---
+++ firebase-storage-test/firebase_storage_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_storage_testApp.swift
+//  firebase-storage-test
+//
+//  Created by Oleksandr Rolik on 09.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct firebase_storage_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
