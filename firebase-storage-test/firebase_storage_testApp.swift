b865edcac18f7440ffefa5e06f25090d1956ee1d
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-09 22:36:20 +0300

    configure firebase

--- firebase-storage-test/firebase_storage_testApp.swift
+++ firebase-storage-test/firebase_storage_testApp.swift
@@ -1,17 +1,24 @@
-//
-//  firebase_storage_testApp.swift
-//  firebase-storage-test
-//
-//  Created by Oleksandr Rolik on 09.06.2025.
-//
-
 import SwiftUI
+import Firebase
 
 @main
 struct firebase_storage_testApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase")
+        
+        return true
     }
 }
